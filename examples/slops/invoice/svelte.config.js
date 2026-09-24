aa35d4279f35037177c685ddf5d13de665954339
Jordan Howlett <jordan@howlett.io>
2026-09-23 21:58:48 -0600

    Make template dependencies reproducible on clean installs

--- examples/slops/invoice/svelte.config.js
+++ examples/slops/invoice/svelte.config.js
@@ -1,3 +1,2 @@
-import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
-
-export default { preprocess: vitePreprocess() };
+// Svelte 5 handles this app's TypeScript without a Vite preprocessor.
+export default {};
